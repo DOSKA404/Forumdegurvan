978358c7d92292c7a70b981f078b1a755c76ad30
GurvanN22 <gurvan.nicolas007@icloud.com>
2023-05-24 11:35:14 +0200

    update login page

--- front/assets/js/script.js
+++ front/assets/js/script.js
@@ -1,11 +1,32 @@
+//components
 import { Login } from "./Components/login.js";
 import { Footer } from "./Components/footer.js";
+// functions
+import { Sup } from "./function/Sup.js"
 
-let linksCss = []
-
-const LoginPage = () => {
-    Login();
-    Footer();
+const LoginPage = (fail) => {
+    Login(fail)
+    document.getElementById('LoginButton').addEventListener('click', () => {
+        Connnexion(document.getElementById('email').value,document.getElementById('psw').value)
+    })
+    Footer()
 }
 
-LoginPage();
+const Connnexion = (email,psw) => {
+    console.log(email , psw )
+    /*fetch('http://localhost:6969/login')
+    .then(response => response.json())
+    .then(data => console.log(data))*/
+    //Todo link to db
+    const Respond = false
+
+    if(Respond){
+        console.log('connect')
+        //todo connect
+    } else {
+        Sup()
+        LoginPage(true)
+    }
+}
+
+LoginPage()
