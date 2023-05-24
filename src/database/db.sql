3adfcbb779592ea3dd14cba4e71348cdd93ecea3
TomJegou <tomjegou10@gmail.com>
2023-05-24 09:41:26 +0200

    wip

--- src/database/db.sql
+++ src/database/db.sql
@@ -11,3 +11,11 @@ create TABLE IF NOT EXISTS Post (
     ID_USER INTEGER,
     FOREIGN KEY(ID_USER) REFERENCES User(ID_USER)
 );
+
+-- Development
+insert into User VALUES(1, "Tom", "Kitten");
+insert into User VALUES(2, "Tyv", "dog");
+insert into User VALUES(3, "Gurvan", "123");
+insert into User VALUES(4, "Banos", "azerty");
+
+insert into Post VALUES(1, "2023-05-24-9-37-10", "I love hotdogs", 2);
