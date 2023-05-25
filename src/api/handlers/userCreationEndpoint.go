63b9cbe6c2ffa2ba1fcc8d8f77ace2b8e2dec495
TomJegou <tomjegou10@gmail.com>
2023-05-25 08:49:21 +0200

    unmarshall user from fetch

--- src/api/handlers/userCreationEndpoint.go
+++ src/api/handlers/userCreationEndpoint.go
@@ -1,7 +1,28 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/GurvanN22/Forum/src/Backend/tools"
+)
+
+type User struct {
+	Email       string
+	Username    string
+	DateOfBirth string
+	Hash        string
+}
 
 func UserCreationHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("user creation endpoint"))
+	if r.Method == http.MethodPost {
+		var user *User
+		byteUser, err := io.ReadAll(r.Body)
+		tools.HandlerError(err)
+		err = json.Unmarshal(byteUser, &user)
+		tools.HandlerError(err)
+	} else {
+		w.Write([]byte("Bad request Method"))
+	}
 }
