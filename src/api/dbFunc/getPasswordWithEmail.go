f49ba659c6e3aa5cc190a425176c122bb914e8d5
TomJegou <tomjegou10@gmail.com>
2023-06-01 08:48:08 +0200

    wip

--- src/api/dbFunc/getPasswordWithEmail.go
+++ src/api/dbFunc/getPasswordWithEmail.go
@@ -3,13 +3,18 @@ package dbFunc
 import (
 	"database/sql"
 
-	"github.com/GurvanN22/Forum/src/Backend/tools"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func GetPasswordWithEmail(email string) error {
+func GetPasswordWithEmail(email string) (string, error) {
+	result := ""
 	db, err := sql.Open("sqlite3", "database/db.db")
-	tools.HandlerError(err)
+	if err != nil {
+		db.Close()
+		return "", err
+	}
 	defer db.Close()
-	return nil
+	row := db.QueryRow("SELECT password_hash FROM User WHERE email=?", email)
+	row.Scan(&result)
+	return result, nil
 }
--- src/api/handlers/userLogin.go
+++ src/api/handlers/userLogin.go
@@ -2,9 +2,11 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 
+	"github.com/GurvanN22/Forum/src/Backend/api/dbFunc"
 	"github.com/GurvanN22/Forum/src/Backend/api/structures"
 	"github.com/GurvanN22/Forum/src/Backend/tools"
 )
@@ -14,8 +16,15 @@ func UserLoginHandlers(w http.ResponseWriter, r *http.Request) {
 		var user *structures.User
 		byteReq, err := io.ReadAll(r.Body)
 		tools.HandlerError(err)
+		fmt.Println(string(byteReq))
 		err = json.Unmarshal(byteReq, user)
 		tools.HandlerError(err)
+		hashPassInDb, err := dbFunc.GetPasswordWithEmail(user.Email)
+		tools.HandlerError(err)
+		tools.CheckPasswordValidity(hashPassInDb, user.Password)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+		}
 	} else {
 		w.Write([]byte("Bad request method"))
 	}
