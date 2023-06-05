0f3cb4fa86f025c159eaa29623b9970f76ddd9f4
TomJegou <tomjegou10@gmail.com>
2023-06-05 09:20:22 +0200

    finish endpoit get post

--- backend/api/handlers/getPost.go
+++ backend/api/handlers/getPost.go
@@ -1,7 +1,20 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/GurvanN22/Forum/src/Backend/api/dbFunc"
+	"github.com/GurvanN22/Forum/src/Backend/tools"
+)
 
 func GetPosts(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("POOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOSTS"))
+	if r.Method == http.MethodGet {
+		listPost := dbFunc.GetPosts()
+		byteListPost, err := json.Marshal(&listPost)
+		tools.HandlerError(err)
+		w.Write(byteListPost)
+	} else {
+		w.Write([]byte("Bad request method"))
+	}
 }
