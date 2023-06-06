cc6f187e34acc94f29532ad758d156d7c5639505
loretban <esteban.loret@ynov.com>
2023-06-06 11:17:08 +0200

    merge the main

--- frontV2/server/server.go
+++ frontV2/server/server.go
@@ -12,7 +12,7 @@ func StartServer() {
 	styles := http.FileServer(http.Dir("assets"))
 	http.Handle("/assets/", http.StripPrefix("/assets", styles)) //We link the css with http.Handle
 
-	http.HandleFunc("/", handlers.Login)
+	http.HandleFunc("/", handlers.MainPage)
 	http.HandleFunc("/register", handlers.Register)
 	http.HandleFunc("/registerResponse", handlers.RegisterResponse)
 	http.HandleFunc("/login", handlers.Login)
@@ -25,14 +25,3 @@ func StartServer() {
 	http.ListenAndServe(":8080", nil) //We start the server on 8080
 
 }
-<<<<<<< HEAD
-func page(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("html/main.html")) //We link the template and the html file
-	tmpl.Execute(w, nil)
-}
-func register(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("html/createAccount.html")) //We link the template and the html file
-	tmpl.Execute(w, nil)
-}
-=======
->>>>>>> main
