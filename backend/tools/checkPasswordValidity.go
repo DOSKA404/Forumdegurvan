79a4187e517ac557545da808717924bfd32316da
TomJegou <tomjegou10@gmail.com>
2023-06-06 08:36:28 +0200

    clean new line

--- backend/tools/checkPasswordValidity.go
+++ backend/tools/checkPasswordValidity.go
@@ -6,9 +6,7 @@ import (
 )
 
 func CheckPasswordValidity(hashInDb string, userInput string) error {
-	
 	hashedUserInput := hex.EncodeToString(HashPassword(userInput))
-
 	for i := range hashInDb {
 		if  hashInDb[i] != hashedUserInput[i] {
 		return fmt.Errorf("incorrect password")
