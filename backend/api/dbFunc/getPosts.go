ebcf18d470fbdaf8b4f8608d352650563cd007e4
TomJegou <tomjegou10@gmail.com>
2023-06-06 11:52:17 +0200

    wip

--- backend/api/dbFunc/getPosts.go
+++ backend/api/dbFunc/getPosts.go
@@ -8,15 +8,20 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func GetPosts() []structures.Post {
+type postWithLike struct {
+	*structures.Post
+	Likes int
+}
+
+func GetPosts() []postWithLike {
 	db, err := sql.Open("sqlite3", "database/db.db")
 	tools.HandlerError(err)
 	defer db.Close()
 	rows, err := db.Query("SELECT Post.id_post, Post.content, User.Username, User.id_user FROM Post INNER JOIN User ON Post.id_user=User.id_user;")
 	tools.HandlerError(err)
-	result := []structures.Post{}
+	result := []postWithLike{}
 	for rows.Next() {
-		var post structures.Post
+		var post postWithLike
 		err = rows.Scan(&post.Id_post, &post.Content, &post.Username, &post.Id_user)
 		tools.HandlerError(err)
 		result = append(result, post)
