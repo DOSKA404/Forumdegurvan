a7446d373ea8008e00267c04093fd8570d43fe3e
TomJegou <tomjegou10@gmail.com>
2023-06-08 09:05:12 +0200

    create like done

---
+++ backend/api/handlers/likePost.go
@@ -0,0 +1,25 @@
+package handlers
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/GurvanN22/Forum/src/Backend/api/dbFunc"
+	"github.com/GurvanN22/Forum/src/Backend/api/structures"
+	"github.com/GurvanN22/Forum/src/Backend/tools"
+)
+
+func LikePost(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		byteBody, err := io.ReadAll(r.Body)
+		tools.HandlerError(err)
+		var likeSendByTheFront structures.LikeSentByTheFront
+		err = json.Unmarshal(byteBody, &likeSendByTheFront)
+		tools.HandlerError(err)
+		dbFunc.CreateLike(&likeSendByTheFront)
+		w.Write([]byte("Like created"))
+	} else {
+		w.Write([]byte("Bad request method"))
+	}
+}
