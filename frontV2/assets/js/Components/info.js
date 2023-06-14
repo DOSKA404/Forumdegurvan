521bdc688886ac32ce3ebff4c76a0611b2e8ad20
GurvanN22 <gurvan.nicolas007@icloud.com>
2023-06-14 09:36:20 +0200

    add bin folder

--- frontV2/assets/js/Components/info.js
+++ frontV2/assets/js/Components/info.js
@@ -13,3 +13,9 @@ document.getElementById("area").addEventListener("input", function (e) {
 function date(Date) {
     return Date[6] + Date[7] + Date[8] + Date[9]+"-"+Date[3] + Date[4]+"-"+Date[0] + Date[1];
 }
+
+document.getElementById("openAccountOptions").addEventListener("click", () => {
+    document.getElementById("AccountOption").classList.toggle("hidden");
+    document.getElementById("AccountOption").classList.toggle("AccountOption");
+    document.getElementById("openAccountOptions").classList.toggle("margin-left");
+});
