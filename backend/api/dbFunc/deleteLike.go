f6e1428f0c8b2388377decc9ed46f0c6e261786b
typhnz <tyvdphsn@gmail.com>
2023-06-15 11:07:46 +0200

    delete like logic

---
+++ backend/api/dbFunc/deleteLike.go
@@ -0,0 +1,23 @@
+package dbFunc
+
+import (
+	"database/sql"
+
+	"github.com/GurvanN22/Forum/src/Backend/api/structures"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func DeleteLike(l *structures.LikeSentByTheFront) error {
+	//establish connection with the database
+	db, err := sql.Open("sqlite3", "database/db.db")
+	if err != nil {
+		return err
+	}
+	//execute the query
+	_, err = db.Exec("DELETE FROM Like WHERE id_post=? AND id_user=?", l.IdPost, l.IdUser)
+	if err != nil {
+		return err
+	}
+	return nil
+}
