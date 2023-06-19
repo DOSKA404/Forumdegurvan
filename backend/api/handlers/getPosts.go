6b04ea9b126fba2f8f1a765a936aaeb091898cd3
TomJegou <tomjegou10@gmail.com>
2023-06-19 09:25:31 +0200

    implement isLiked logic

--- backend/api/handlers/getPosts.go
+++ backend/api/handlers/getPosts.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strconv"
 
 	"github.com/GurvanN22/Forum/src/Backend/api/dbFunc"
 	"github.com/GurvanN22/Forum/src/Backend/tools"
@@ -13,7 +14,10 @@ func GetPosts(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		byteBody, err := io.ReadAll(r.Body)
 		tools.HandlerError(w, err)
-		listPost, err := dbFunc.GetPosts(string(byteBody))
+		tmp := string(byteBody)
+		idUser, err := strconv.Atoi(tmp)
+		tools.HandlerError(w, err)
+		listPost, err := dbFunc.GetPosts(idUser)
 		tools.HandlerError(w, err)
 		byteListPost, err := json.Marshal(&listPost)
 		tools.HandlerError(w, err)
