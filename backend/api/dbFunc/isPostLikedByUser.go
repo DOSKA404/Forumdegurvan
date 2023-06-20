f59564eb6e5ff2e7242e44526348461e4e4d98cf
TomJegou <tomjegou10@gmail.com>
2023-06-20 09:26:52 +0200

    correct like

--- backend/api/dbFunc/isPostLikedByUser.go
+++ backend/api/dbFunc/isPostLikedByUser.go
@@ -1,10 +1,12 @@
 package dbFunc
 
-import "database/sql"
+import (
+	"database/sql"
+)
 
 func IsPostLikedByUser(idUser, idPost int, db *sql.DB) bool {
 	var result int = 0
 	row := db.QueryRow("SELECT COUNT(id_like) FROM Like WHERE id_user=? AND id_post=?", idUser, idPost)
-	row.Scan(result)
+	row.Scan(&result)
 	return result == 1
 }
